76ec9fc88c68d61bde436a086ac3c56228b91728
FishInWater-1999 <814484626@qq.com>
2020-06-25 15:53:46 +0800

    UPD：flutter application 转 module
    UPD：flutter application 转 module

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:p01_flutter_proj/main.dart';
+import 'package:p01flutterproj/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
